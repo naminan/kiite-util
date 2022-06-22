7d2cdc993e02d44aefd2a1b6c7201d243f8c239e
naminan <107912851+naminan@users.noreply.github.com>
2022-06-22 17:25:56 +0900

    公式で対応されたので、人数表示削除

--- src/kiitecafe.user.js
+++ src/kiitecafe.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         KiiteCafeCustom
 // @namespace    https://github.com/naminan/
-// @version      0.1
-// @description  KiiteCafeに動画再生数とCafe人数を表示します
+// @version      0.2
+// @description  KiiteCafeに動画再生数を表示します
 // @author       nami
 // @match        https://cafe.kiite.jp/pc
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cafe.kiite.jp
@@ -19,20 +19,5 @@
             t.artist_name += ' ' + Number(t.baseinfo.view_counter).toLocaleString() + '再生';
             this.update_now_playing_info_(t)
         }
-        const peopleDiv = document.createElement('div')
-        window.CafeUsers.prototype.plot_users_ = window.CafeUsers.prototype.plot_users
-        window.CafeUsers.prototype.plot_users = function (t) {
-            peopleDiv.innerHTML　= '<i class="material-icons" style="font-size:13px;margin-right:3px;">account_circle</i>' + this.users.length
-            this.plot_users_()
-        }
-        document.getElementsByClassName("cafe_users")[0].appendChild(peopleDiv)
-        peopleDiv.style.position = 'absolute'
-        peopleDiv.style.right = '0px'
-        peopleDiv.style.bottom = '0px'
-        peopleDiv.style.background = 'black'
-        peopleDiv.style.padding = '2px 4px'
-        peopleDiv.style.opacity = '0.3'
-        peopleDiv.style.display = 'inline-flex'
-        peopleDiv.style['font-size'] = '12px'
     }
 })();
